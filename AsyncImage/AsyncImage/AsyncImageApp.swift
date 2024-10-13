4a00a0574decb5d197b14c7586eda6a58533fdda
haroldstest <dongik369@naver.com>
2024-10-14 03:24:06 +0900

    AsyncImage section Fin

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Dongik Song on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
